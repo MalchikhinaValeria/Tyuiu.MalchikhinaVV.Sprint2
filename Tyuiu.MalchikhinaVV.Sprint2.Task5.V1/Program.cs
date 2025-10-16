e9a6ece9fa1f6d739d720a8762b7886b757555c5
Home <Home@LAPTOP-L43VJUUA>
2025-10-16 17:13:52 +0500

    Task5

---
+++ Tyuiu.MalchikhinaVV.Sprint2.Task5.V1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MalchikhinaVV.Sprint2.Task5.V1.Lib;
+
+namespace Tyuiu.MalchikhinaVV.Sprint2.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #1 | Выполнил: Мальчихина В. В. | ИИПб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Мальчихина Валерия Викторовна | ИИПб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, использует оператор switch и                        *");
+            Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца:  ");
+            int numMonth = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+            if ((numMonth < 1) || (numMonth > 12))
+            {
+                res = "Введено неверное значение";
+
+            }
+            else
+            {
+                res = "В этом месяце " + ds.FindMonthDaysCount(numMonth) + " дней";
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine(res);
+
+            Console.ReadLine();
+        }
+    }
+}
+
