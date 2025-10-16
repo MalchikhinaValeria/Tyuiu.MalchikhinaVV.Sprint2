bdeb08b90cd3070eddae24f5d9765bb656411070
Home <Home@LAPTOP-L43VJUUA>
2025-10-16 16:02:42 +0500

    Task4

---
+++ Tyuiu.MalchikhinaVV.Sprint2.Task4.V4.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MalchikhinaVV.Sprint2.Task4.V4.Lib;
+namespace Tyuiu.MalchikhinaVV.Sprint2.Task4.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 6;
+            double res = ds.Calculate(x, y);
+            double wait = 12.141;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 23.346;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MalchikhinaVV.Sprint2.Task4.V4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MalchikhinaVV.Sprint2.Task4.V4.Lib;
+
+namespace Tyuiu.MalchikhinaVV.Sprint2.Task4.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #1 | Выполнил: Мальчихина В. В. | ИИПб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Мальчихина Валерия Викторовна | ИИПб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение                *");
+            Console.WriteLine("* с использованием тернарного оператора,                                  *");
+            Console.WriteLine("* где пользователь вводит значение переменной х, у с клавиатуры.          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной Х:  ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:  ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x,y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadLine();
+        }
+    }
+}
+
